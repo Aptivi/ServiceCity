905a1ff3e3ea033d03982393c934bc0d2929d207
Aptivi <eoflaoevicecity@gmail.com>
2025-01-21 13:00:53 +0300

    add - Added support for Linux
    ---
    
    We've added preliminary support for Linux.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- public/ServiceCity/Tools/Native/NativeServiceEnumerator.Linux.cs
+++ public/ServiceCity/Tools/Native/NativeServiceEnumerator.Linux.cs
@@ -18,7 +18,12 @@
 //
 
 using ServiceCity.Instances;
+using SpecProbe.Software.Platform;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Textify.General;
 
 namespace ServiceCity.Tools.Native
 {
@@ -26,8 +31,65 @@ namespace ServiceCity.Tools.Native
     {
         internal static SystemService[] EnumerateServicesLinux()
         {
-            // TODO: Placeholder
             List<SystemService> services = [];
+
+            // Check for systemd
+            string[] systemctls = PlatformHelper.GetPossiblePaths("systemctl");
+            if (systemctls.Length > 0)
+            {
+                string systemctl = systemctls[0];
+
+                // Systemd is found! Use systemctl to query services and their status
+                string[] serviceUnitLines = PlatformHelper.ExecuteProcessToString(systemctl, "list-units --full --all -t service --no-legend --plain").SplitNewLines();
+                foreach (string line in serviceUnitLines)
+                {
+                    if (string.IsNullOrEmpty(line))
+                        continue;
+                    string[] fields = line.Split([' '], StringSplitOptions.RemoveEmptyEntries);
+
+                    // Get the field values
+                    string name = fields[0];
+                    string displayName = string.Join(" ", fields.Skip(4));
+
+                    // Generate a service instance
+                    var service = new SystemService()
+                    {
+                        Name = name,
+                        DisplayName = displayName,
+                    };
+                    services.Add(service);
+                }
+            }
+
+            // Check old-style init.d files
+            string[] initdServices = Directory.GetFiles("/etc/init.d");
+            foreach (string servicePath in initdServices)
+            {
+                string name = Path.GetFileNameWithoutExtension(servicePath);
+                string displayName = "";
+
+                // Read the name from the script
+                using var scriptStream = File.OpenRead(servicePath);
+                using var scriptStreamReader = new StreamReader(scriptStream);
+                while (!scriptStreamReader.EndOfStream)
+                {
+                    string line = scriptStreamReader.ReadLine();
+                    if (line == "### END INIT INFO")
+                        break;
+
+                    // Search for display name
+                    if (line.StartsWithNoCase("# Short-Description:"))
+                        displayName = line.RemovePrefix("# Short-Description:").Trim();
+                }
+
+                // Generate a service instance
+                var service = new SystemService()
+                {
+                    Name = name,
+                    DisplayName = displayName,
+                };
+                services.Add(service);
+            }
             return [.. services];
         }
     }
