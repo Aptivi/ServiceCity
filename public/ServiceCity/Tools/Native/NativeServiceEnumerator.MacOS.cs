6b1d6a1e93756383010006123fb653b6ffb9b648
Aptivi <eoflaoevicecity@gmail.com>
2025-01-24 14:42:23 +0300

    add - Added launchctl support
    ---
    
    For macOS, we've added launchctl.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- public/ServiceCity/Tools/Native/NativeServiceEnumerator.MacOS.cs
+++ public/ServiceCity/Tools/Native/NativeServiceEnumerator.MacOS.cs
@@ -18,7 +18,11 @@
 //
 
 using ServiceCity.Instances;
+using SpecProbe.Software.Platform;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using Textify.General;
 
 namespace ServiceCity.Tools.Native
 {
@@ -26,8 +30,36 @@ namespace ServiceCity.Tools.Native
     {
         internal static SystemService[] EnumerateServicesMacOS()
         {
-            // TODO: Placeholder
             List<SystemService> services = [];
+
+            // Check for launchctl
+            string[] launchctls = PlatformHelper.GetPossiblePaths("launchctl");
+            if (launchctls.Length > 0)
+            {
+                string launchctl = launchctls[0];
+
+                // launchctl is found! Use launchctl to query services and their status
+                string[] serviceLines = PlatformHelper.ExecuteProcessToString(launchctl, "list").SplitNewLines().Skip(1).ToArray();
+                foreach (string line in serviceLines)
+                {
+                    if (string.IsNullOrEmpty(line))
+                        continue;
+                    string[] fields = line.Split(['\t'], StringSplitOptions.RemoveEmptyEntries);
+
+                    // Get the field values
+                    string name = fields[2];
+                    string displayName = name;
+
+                    // Generate a service instance
+                    var service = new SystemService()
+                    {
+                        Name = name,
+                        DisplayName = displayName,
+                    };
+                    services.Add(service);
+                }
+            }
+
             return [.. services];
         }
     }
